e0c2fbdffc309e301bf5ffb74b6da674e7744a3e
Minigrim0 <grimauflorent@gmail.com>
2024-10-25 14:33:37 +0200

    added frontend application for graph building

---
+++ frustic/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+// import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+
+import { BaklavaEditor, useBaklava } from '@baklavajs/renderer-vue';
+import "@baklavajs/themes/dist/syrup-dark.css";
+
+const baklava = useBaklava();
+</script>
+
+<template>
+    <nav>
+        <h1>Rustic</h1>
+    </nav>
+
+    <div class="editor">
+        <BaklavaEditor :view-model="baklava" />
+    </div>
+
+  <!-- <RouterView /> -->
+</template>
