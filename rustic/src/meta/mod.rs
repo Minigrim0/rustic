aff1c4613b64aef7b866405bc354075e578b36a9
Minigrim0 <grimauflorent@gmail.com>
2025-04-08 17:25:40 +0900

    Added metadata export function

--- rustic/src/meta/mod.rs
+++ rustic/src/meta/mod.rs
@@ -1,3 +1,25 @@
-mod traits;
+pub mod structs;
+pub mod traits;
 
-pub use traits::FilterMetadata;
+use structs::MetaFilter;
+
+#[macro_export]
+macro_rules! filters {
+    ( $( $x:ident ),* ) => {
+        {
+            let mut temp_vec = Vec::new();
+            $(
+                temp_vec.push(Box::new($x::default()) as Box<dyn Filter>);
+            )*
+            temp_vec
+        }
+    };
+}
+
+pub fn get_filters() -> Vec<MetaFilter> {
+    vec![
+        crate::core::filters::prelude::GainFilter_META().clone(),
+        crate::core::filters::prelude::Clipper_META().clone(),
+        crate::core::filters::prelude::CombinatorFilter_META().clone(),
+    ]
+}
--- rustic/src/meta/traits.rs
+++ rustic/src/meta/traits.rs
@@ -1,6 +1,15 @@
-pub trait FilterMetadata {
+use crate::core::graph::Filter;
+use rustic_meta::Parameter;
+
+pub trait FilterFactory: Send + Sync {
+    fn create_instance(&self) -> Box<dyn Filter>;
+}
+
+pub trait FilterMetadata: Send + Sync {
     fn name(&self) -> String;
     fn description(&self) -> String;
     fn source_amount(&self) -> usize;
-    fn parameters(&self) -> Vec<rustic_meta::Parameter>;
+    fn parameters(&self) -> Vec<Parameter<&'static str>>;
 }
+
+pub trait FrontendFilter: FilterMetadata + FilterFactory {}
