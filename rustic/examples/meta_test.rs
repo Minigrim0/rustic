15069b56c6cb439af0faf121b5ad09be53033e46
Minigrim0 <fgrimau@proton.me>
2026-01-09 15:34:26 +0000

    [Fix] meta test not running when feature meta is off

--- rustic/examples/meta_test.rs
+++ rustic/examples/meta_test.rs
@@ -1,6 +1,13 @@
-use rustic::core::filters::prelude::{HighPassFilter_META, LowPassFilter_META};
 
+#[cfg(not(feature = "meta"))]
 fn main() {
+    println!("This example can only run with the 'meta' feature flag");
+}
+
+#[cfg(feature = "meta")]
+fn main() {
+    use rustic::core::filters::prelude::{HighPassFilter_META, LowPassFilter_META};
+
     let filter = LowPassFilter_META().clone();
     let filter2 = HighPassFilter_META().clone();
 
