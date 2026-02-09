428190d07252eee29e9a4d2070c03c2772cef7a5
Minigrim0 <fgrimau@proton.me>
2026-02-09 15:41:14 +0000

    [Add] Generators & Sinks metadata

--- rustic/src/meta/mod.rs
+++ rustic/src/meta/mod.rs
@@ -1,7 +1,9 @@
+use rustic_meta::Parameter;
+
 pub mod structs;
 pub mod traits;
 
-use structs::MetaFilter;
+use structs::{MetaFilter, MetaGenerator, MetaSink};
 
 #[macro_export]
 macro_rules! filters {
@@ -18,8 +20,55 @@ macro_rules! filters {
 
 pub fn get_filters() -> Vec<MetaFilter> {
     vec![
-        crate::core::filters::prelude::GainFilter_META().clone(),
-        crate::core::filters::prelude::Clipper_META().clone(),
-        crate::core::filters::prelude::CombinatorFilter_META().clone(),
+        crate::core::filters::prelude::GainFilter_META(),
+        crate::core::filters::prelude::Clipper_META(),
+        crate::core::filters::prelude::CombinatorFilter_META(),
+        crate::core::filters::prelude::Tremolo_META(),
+        crate::core::filters::prelude::DelayFilter_META(),
+        crate::core::filters::prelude::LowPassFilter_META(),
+        crate::core::filters::prelude::HighPassFilter_META(),
+        crate::core::filters::prelude::BandPass_META(),
+        crate::core::filters::prelude::ResonantBandpassFilter_META(),
+        crate::core::filters::prelude::MovingAverage_META(),
+        crate::core::filters::prelude::DuplicateFilter_META(),
+        crate::core::filters::prelude::Compressor_META(),
     ]
+}
+
+pub fn get_generators() -> Vec<MetaGenerator> {
+    vec![
+        MetaGenerator {
+            name: "Sine Wave",
+            description: "Generates a pure sine wave signal",
+            parameters: vec![
+                Parameter::Range {
+                    title: "Frequency",
+                    field_name: "frequency",
+                    min: 20.0,
+                    max: 20000.0,
+                    default: 440.0,
+                    value: 440.0,
+                },
+                Parameter::Range {
+                    title: "Amplitude",
+                    field_name: "amplitude",
+                    min: 0.0,
+                    max: 1.0,
+                    default: 0.5,
+                    value: 0.5,
+                },
+            ],
+            output_count: 1,
+        },
+        // Square Wave, Sawtooth Wave, Triangle Wave, White Noise
+        // Same structure, different name/description
+    ]
+}
+
+pub fn get_sinks() -> Vec<MetaSink> {
+    vec![MetaSink {
+        name: "Audio Output",
+        description: "Outputs audio to the system audio device",
+        input_count: 1,
+    }]
 }
--- rustic/src/meta/structs.rs
+++ rustic/src/meta/structs.rs
@@ -2,6 +2,21 @@ use rustic_meta::Parameter;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct MetaGenerator {
+    pub name: &'static str,
+    pub description: &'static str,
+    pub parameters: Vec<Parameter<&'static str>>,
+    pub output_count: usize,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct MetaSink {
+    pub name: &'static str,
+    pub description: &'static str,
+    pub input_count: usize,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct MetaFilter {
     pub name: &'static str,
     pub description: &'static str,
