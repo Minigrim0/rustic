376fa38d7a6deb232b9bb9664b77f2046df75927
Minigrim0 <fgrimau@proton.me>
2026-02-19 18:32:28 +0000

    [Upd] Update examples for Block-based graph API

--- rustic/examples/meta_test.rs
+++ rustic/examples/meta_test.rs
@@ -1,13 +1,5 @@
-#[cfg(not(feature = "meta"))]
-fn main() {
-    println!("This example can only run with the 'meta' feature flag");
-}
-
-#[cfg(feature = "meta")]
 fn main() {
     use rustic::core::filters::prelude::{HighPassFilter, LowPassFilter};
-
-    // MetaFilter trait must be in scope for ::metadata() calls
     use rustic_meta::MetaFilter;
 
     let filter = LowPassFilter::metadata();
@@ -16,12 +8,12 @@ fn main() {
     println!("Metadata:");
     println!("\tName: {}", filter.name);
     println!("\tDescription: {}", filter.description);
-    println!("\tSource amount: {}", filter.source_amount);
-    println!("\tParameters: {:?}", filter.parameters);
+    println!("\tAudio inputs: {}", filter.inputs.iter().filter(|i| i.parameter.is_none()).count());
+    println!("\tParameters: {:?}", filter.inputs.iter().filter(|i| i.parameter.is_some()).collect::<Vec<_>>());
 
     println!("Metadata:");
     println!("\tName: {}", filter2.name);
     println!("\tDescription: {}", filter2.description);
-    println!("\tSource amount: {}", filter2.source_amount);
-    println!("\tParameters: {:?}", filter2.parameters);
+    println!("\tAudio inputs: {}", filter2.inputs.iter().filter(|i| i.parameter.is_none()).count());
+    println!("\tParameters: {:?}", filter2.inputs.iter().filter(|i| i.parameter.is_some()).collect::<Vec<_>>());
 }
