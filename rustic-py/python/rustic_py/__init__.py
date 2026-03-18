1a041ac9430beeafcd5152ef7114a700677df807
Minigrim0 <fgrimau@proton.me>
2026-03-18 15:11:15 +0000

    [Upd] imports visibility + removed pycache files

--- rustic-py/python/rustic_py/__init__.py
+++ rustic-py/python/rustic_py/__init__.py
@@ -1,2 +1,8 @@
 from ._classes import SourceSpec, GraphSpec, filter_classes
+import sys as _sys
+
+# Re-export filters
+_mod = _sys.modules[__name__]
+for _type_id, _cls in filter_classes.items():
+    setattr(_mod, _cls.__name__, _cls)
 
