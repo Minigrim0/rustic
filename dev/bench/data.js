b6e1e18436bb08e59639edd70250aed125ec5c25
github-action-benchmark <github@users.noreply.github.com>
2026-04-01 10:17:56 +0000

    add Benchmark (cargo) benchmark result for 3bc7f2e3db51b60dc48b9e74078ecc1bc2c52702

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1775038675799,
+  "repoUrl": "https://github.com/Minigrim0/rustic",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "44365630+Minigrim0@users.noreply.github.com",
+            "name": "Florent Grimau",
+            "username": "Minigrim0"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3bc7f2e3db51b60dc48b9e74078ecc1bc2c52702",
+          "message": "Merge pull request #16 from Minigrim0/core/dev\n\nML Autoregression + Py-bindings v0.2.0",
+          "timestamp": "2026-04-01T12:15:22+02:00",
+          "tree_id": "5fc28440fbbb7b606cbd4890ba11d653afe92037",
+          "url": "https://github.com/Minigrim0/rustic/commit/3bc7f2e3db51b60dc48b9e74078ecc1bc2c52702"
+        },
+        "date": 1775038675118,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "simple_graph",
+            "value": 316745741,
+            "range": "± 8321618",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "complex_graph",
+            "value": 245312804,
+            "range": "± 1261315",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
